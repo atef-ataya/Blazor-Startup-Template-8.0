640278985ba404fe9d79eaf86b18f34c862a42ae
Atef Ataya <atef.ataya@gmail.com>
2024-01-23 08:32:57 +0400

    Adding Cache Service

---
+++ Server/Services/CacheService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Concurrent;
+
+namespace Server.Services
+{
+    public class CacheService
+    {
+        private readonly ConcurrentDictionary<string, object> _cache = new ConcurrentDictionary<string, object>();
+        private readonly TimeSpan _cacheDuration = TimeSpan.FromMinutes(30); // Adjust as needed
+
+        public async Task<T> GetAsync<T>(string key)
+        {
+            if (_cache.TryGetValue(key, out var cachedItem))
+            {
+                var item = (CachedItem<T>)cachedItem;
+                if (item.ExpirationTime == null || item.ExpirationTime > DateTimeOffset.Now)
+                {
+                    return item.Value;
+                }
+                else
+                {
+                    _cache.TryRemove(key, out _);
+                }
+            }
+
+            return default(T);
+        }
+
+        public async Task SetAsync<T>(string key, T value)
+        {
+            var item = new CachedItem<T> { Value = value, ExpirationTime = DateTimeOffset.Now.Add(_cacheDuration) };
+            _cache[key] = item;
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return _cache.ContainsKey(key);
+        }
+
+        private class CachedItem<T>
+        {
+            public T Value { get; set; }
+            public DateTimeOffset? ExpirationTime { get; set; }
+        }
+    }
+}
--- Server/Services/StateContainer.cs
+++ Server/Services/StateContainer.cs
@@ -2,5 +2,24 @@
 {
     public class StateContainer
     {
+        private string? savedString;
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        public string SavedString
+        {
+            get => savedString ?? string.Empty;
+            set
+            {
+                savedString = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public void ResetState()
+        {
+
+        }
     }
 }
