131ee76057f7f70635a98953a17364fc80c0a937
Atef Ataya <atef.ataya@gmail.com>
2024-01-23 08:26:48 +0400

    Adding Check Image Type

---
+++ Server/Utilities/CheckImageType.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Linq;
+
+namespace Server.Utilities
+{
+    public class CheckImageType
+    {
+        public bool IsImageFile(IBrowserFile file)
+        {
+            string[] permittedExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+            var extension = Path.GetExtension(file.Name).ToLowerInvariant();
+            return permittedExtensions.Contains(extension);
+        }
+    }
+}
