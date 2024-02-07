8aa719422f9ebaaa357c1e66f108ca427db6c8cc
Atef Ataya <atef.ataya@gmail.com>
2024-02-07 11:29:33 +0400

    Adding DataGrid

---
+++ Server/Models/Element.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Models
+{
+    public class Element
+    {
+        public string Number { get; set; }
+        public string Sign { get; set; }
+        public string Name { get; set; }    
+        public string Position { get; set; }
+        public string Molar { get; set; }
+        public string Group { get; set; }
+    }
+}
